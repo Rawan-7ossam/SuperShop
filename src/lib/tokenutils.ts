54bd8babd2c886b372f492a7cc7235a0822c4e93
Rawan-7ossam <rawanhossam4000@gmail.com>
2025-09-18 02:14:31 +0300

    commit

--- src/lib/tokenutils.ts
+++ src/lib/tokenutils.ts
@@ -3,9 +3,16 @@ import { cookies } from "next/headers";
 
 export async function getUserToken(){
 
-    const encodeToken = (await cookies()).get("next-auth.session-token")?.value;
-    
-        const decodeToken = await  decode({token:encodeToken , secret:process.env.AUTH_SECRET!});
+    const cookieStore =await cookies();
+
+    const encodeToken = 
+    cookieStore.get("next-auth.session-token")?.value ||
+    cookieStore.get("_Secure-next-auth.session-token")?.value;
+
+        if(!encodeToken){
+            return undefined;
+        }
+        const decodeToken = await  decode({token:encodeToken , secret:process.env.NEXTAUTH_SECRET!});
     
         const token = decodeToken?.token
 
